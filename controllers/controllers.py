4bfc8c1017be0b58f43f9f7b2168b7a0c0918700
naoufaltokane <naoufaltokane@gmail.com>
2020-10-21 10:46:07 +0100

    v2.00 (demo)

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,21 +1,49 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http, _
+from odoo.http import request
+from odoo.addons.stock_barcode.controllers.main import StockBarcodeController
 
+class StockBarcodeController(StockBarcodeController):
 
-# class BarcodeArticleInformation(http.Controller):
-#     @http.route('/barcode_article_information/barcode_article_information/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+    @http.route('/stock_barcode/scan_from_main_menu', type='json', auth='user')
+    def main_menu(self, barcode, **kw):
+        """ Receive a barcode scanned from the main menu and return the appropriate
+            action (open an existing / new picking) or warning.
+        """
+        ret_open_picking = self.try_open_picking(barcode)
+        if ret_open_picking:
+            return ret_open_picking
 
-#     @http.route('/barcode_article_information/barcode_article_information/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('barcode_article_information.listing', {
-#             'root': '/barcode_article_information/barcode_article_information',
-#             'objects': http.request.env['barcode_article_information.barcode_article_information'].search([]),
-#         })
+        ret_open_picking_type = self.try_open_picking_type(barcode)
+        if ret_open_picking_type:
+            return ret_open_picking_type
 
-#     @http.route('/barcode_article_information/barcode_article_information/objects/<model("barcode_article_information.barcode_article_information"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('barcode_article_information.object', {
-#             'object': obj
-#         })
+        if request.env.user.has_group('stock.group_stock_multi_locations'):
+            ret_new_internal_picking = self.try_new_internal_picking(barcode)
+            if ret_new_internal_picking:
+                return ret_new_internal_picking
+
+        ret_show_product = self.try_show_product(barcode)
+        if ret_show_product:
+            return ret_show_product
+
+        if request.env.user.has_group('stock.group_stock_multi_locations'):
+            return {'warning': _('No picking or location corresponding to barcode %(barcode)s') % {'barcode': barcode}}
+        else:
+            return {'warning': _('No picking corresponding to barcode %(barcode)s') % {'barcode': barcode}}
+
+    def try_show_product(self, barcode):
+        corresponding_product = request.env['product.product'].search([('barcode', '=', barcode),], limit=1)
+        if corresponding_product:
+            view_id = request.env.ref('product.product_template_form_view').id
+            return {
+                'action': {
+                    'name': _('open product form'),
+                    'res_model': 'product.product',
+                    'view_mode': 'form',
+                    'view_id': view_id,
+                    'views': [(view_id, 'form')],
+                    'type': 'ir.actions.act_window',
+                    'res_id': corresponding_product.id,
+                }
+            }
--- models/models.py
+++ models/models.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
-
+# #-*- coding: utf-8 -*-
+#
 # from odoo import models, fields, api
-
-
+#
+#
 # class barcode_article_information(models.Model):
 #     _name = 'barcode_article_information.barcode_article_information'
 #     _description = 'barcode_article_information.barcode_article_information'
-
+#
 #     name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
